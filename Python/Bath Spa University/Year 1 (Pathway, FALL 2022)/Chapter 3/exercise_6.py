d513174a41c63b74a6f50cafec61de4073c8409d
Antonio Intal <59204403+Antonio-III@users.noreply.github.com>
2025-05-20 13:17:32 +0400

    Update exercise_6.py

--- Python/Bath Spa University/Year 1 (Pathway, FALL 2022)/Chapter 3/exercise_6.py
+++ Python/Bath Spa University/Year 1 (Pathway, FALL 2022)/Chapter 3/exercise_6.py
@@ -6,7 +6,7 @@ guests = ["Antonio", "Miguel", "Intal"]
 print(f"My new dinner table couldn't arrive in time, so I can only have {ALLOWED_GUESTS} guests for my dinner!")
 
 # Each time you pop a name from your list, print a message to that person letting them know you’re sorry you can’t invite them to dinner.		
-while len(guests)>ALLOWED_GUESTS:
+while len(guests) > ALLOWED_GUESTS:
 		print(f"I'm so sorry, {guests.pop()}. I can only host 2 guests for my dinner!")
 
 # Print a message to each of the two people still on your list, letting them know they’re still invited.
@@ -14,7 +14,7 @@ for guest in guests:
     print(f"Hello, {guest}! You're still invited to my dinner!")
     
 # Use del to remove the last two names from your list, so you have an empty list.
-while len(guests)!=EMPTY:
+while len(guests) != EMPTY:
 	del guests[0]
 
 # Print your list to make sure you actually have an empty list at the end of your program.
